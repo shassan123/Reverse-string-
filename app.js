094daf5b58ff71f6e786f4c8f8c7b900cf31c7dc
Syed Hassan <hassanaltaf.alphasquad@gmail.com>
2022-12-13 20:00:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// function reverse1(){
+//     let data = document.getElementById("input").value;
+//     data.split("").reverse().join("");
+//     document.getElementById("input").value = data;
+// }
+let data;
+function submit1() {
+  document.getElementById("info").style.display = "block";
+  document.getElementById("btn-2").style.display = "block";
+  document.getElementById("btn-3").style.display = "block";
+  data = document.getElementById("input").value;
+  document.getElementById("info").innerHTML = data;
+  document.getElementById("btn-3").style.display = "none";
+  document.getElementById("input").value = "";
+}
+function rev() {
+  document.getElementById("btn-2").style.display = "none";
+  document.getElementById("btn-3").style.display = "block";
+  document.getElementById("info").innerHTML = data.split("").reverse().join("");
+}
+function refreshPage() {
+  window.location.reload();
+}
+function rev1() {
+    document.getElementById("info").innerHTML = data;
+    document.getElementById("btn-2").style.display = "block";
+    document.getElementById("btn-3").style.display = "none";
+}
